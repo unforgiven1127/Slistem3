0e13fa84978f189de46f1a458e518b29cd760db7
unforgiven1127 <munir_anameric@hotmail.com>
2016-11-14 20:09:44 -0800

    cmplex search

--- updates.php
+++ updates.php
@@ -8,6 +8,8 @@
     define('DB_USER_SLISTEM', 'pma_admin');
     define('DB_PASSWORD_SLISTEM', 'KVW4PVVAWHASuRDz');
 
+    echo "TEST";
+
     mysql_connect( DB_SERVER_SLISTEM, DB_USER_SLISTEM, DB_PASSWORD_SLISTEM) or die(mysql_error());
     mysql_select_db(DB_NAME_SLISTEM) or die(mysql_error());
 
