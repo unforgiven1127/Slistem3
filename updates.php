ff856f7a6c6eaea921034be9709f8e9c21127347
unforgiven1127 <munir_anameric@hotmail.com>
2016-11-16 21:40:39 -0800

    dba

--- updates.php
+++ updates.php
@@ -1,15 +1,33 @@
 <?php
-$to      = 'munir_anameric@hotmail.com';
-$subject = 'the subject';
-$message = 'hello';
-$headers = 'From: webmaster@example.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-$headers .= 'Cc: munir@slate-ghc.com' . "\r\n";
 
+define('CONST_PHPMAILER_SMTP_DEBUG', false);
+define('CONST_PHPMAILER_DOMAIN', 'slate.co.jp');
+define('CONST_PHPMAILER_EMAIL', 'slistem@slate.co.jp');
+define('CONST_PHPMAILER_DEFAULT_FROM', 'Slistem');
+define('CONST_PHPMAILER_ATTACHMENT_SIZE', 10485760);
 
+define('CONST_PHPMAILER_SMTP_PORT', 465); //smtp
+define('CONST_PHPMAILER_SMTP_HOST', 'imap.slate.co.jp');
+define('CONST_PHPMAILER_SMTP_LOGIN', 'slistem@slate.co.jp');
 
-mail($to, $subject, $message, $headers);
+define('CONST_PHPMAILER_SMTP_PASSWORD', 'Slate!7000ics');
 
+require_once "smtp.php";
 
+$from = "slistem@slate.co.jp";
+$to = "munir@slate-ghc.com";
+$subject = "Test subject";
+$message = "Test message, blah blah blah...";
+
+$smtpoptions = array(
+  "textmessage" => $message,
+  "server" => "imap.slate.co.jp",
+  "port" => 465,
+  "secure" => false,
+  "username" => "slistem@slate.co.jp",
+  "password" => "Slate!7000ics",
+  "usedns" => false
+);
+
+SendEmail($from, $to, $subject, $smtpoptions);
 ?>
