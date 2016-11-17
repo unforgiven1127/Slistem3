36cf3b03d3482359aae3253ed7af07ca1b51d45f
unforgiven1127 <munir_anameric@hotmail.com>
2016-11-16 21:28:04 -0800

    dba

--- updates.php
+++ updates.php
@@ -6,7 +6,10 @@ $headers = 'From: webmaster@example.com' . "\r\n" .
     'Reply-To: webmaster@example.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-mail($to, $subject, $message, $headers);
 
-print phpinfo();
+mail('munir_anameric@hotmail.com', 'the subject', 'the message', null, '-fwebmaster@example.com');
+
+//mail($to, $subject, $message, $headers);
+
+
 ?>
