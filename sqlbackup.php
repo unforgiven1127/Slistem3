483385e434cd40854173accffa62929cf1328ab1
unforgiven1127 <munir_anameric@hotmail.com>
2016-11-17 14:13:38 -0800

    dba

--- sqlbackup.php
+++ sqlbackup.php
@@ -12,7 +12,7 @@ echo 'Backup started '.date('Y-m-d h:i:s')."<br><br>";
 //run the MySQL dump and zip;
 
 // location of your temp directory
-$tmpDir = __DIR__."/test/";
+$tmpDir = "/web/backup/";
 // username for MySQL
 echo $tmpDir;
 exit();
