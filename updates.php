f94e4e64a7561e3ac48b8bd7372959a3605f9090
unforgiven1127 <munir_anameric@hotmail.com>
2016-11-16 20:33:36 -0800

    dba

--- updates.php
+++ updates.php
@@ -11,6 +11,7 @@ define('CONST_PHPMAILER_ATTACHMENT_SIZE', 10485760);
 define('CONST_PHPMAILER_SMTP_PORT', 465); //smtp
 define('CONST_PHPMAILER_SMTP_HOST', 'imap.slate.co.jp');
 define('CONST_PHPMAILER_SMTP_LOGIN', 'slistem@slate.co.jp');
+define('CONST_CRM_MAIL_SENDER', 'slistem@slate.co.jp');
 
 define('CONST_PHPMAILER_SMTP_PASSWORD', 'Slate!7000ics');
 
@@ -19,6 +20,18 @@ $sType = "not";
 $nPK = "0";
 $sMode = "ajx";
 
-$mail = new CMailEx($sAction, $sType, $nPK, $sMode);
+$oMail = new CMailEx($sAction, $sType, $nPK, $sMode);
+
+$sSubject = "TEST";
+$sContent = "Test Content";
+
+$sEmail = 'Dear Munir';
+$sEmail.= $sContent;
+
+$oMail->createNewEmail();
+$oMail->setFrom(CONST_PHPMAILER_DEFAULT_FROM, CONST_CRM_MAIL_SENDER);
+$oMail->addRecipient("munir_anameric@hotmail.com");
+
+$oMail->send($sSubject, $sEmail);
 
 ?>
