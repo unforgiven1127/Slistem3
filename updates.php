802ca2dfaa97520b5ade1eb116481d418dba7261
unforgiven1127 <munir_anameric@hotmail.com>
2016-11-16 20:30:09 -0800

    dba

--- updates.php
+++ updates.php
@@ -2,6 +2,16 @@
 
 require_once 'component/mail/mail.class.ex.php5';
 
+define('CONST_PHPMAILER_SMTP_DEBUG', false);
+define('CONST_PHPMAILER_DOMAIN', 'slate.co.jp');
+define('CONST_PHPMAILER_EMAIL', 'slistem@slate.co.jp');
+define('CONST_PHPMAILER_DEFAULT_FROM', 'Slistem');
+define('CONST_PHPMAILER_ATTACHMENT_SIZE', 10485760);
+
+define('CONST_PHPMAILER_SMTP_PORT', 465); //smtp
+define('CONST_PHPMAILER_SMTP_HOST', 'imap.slate.co.jp');
+define('CONST_PHPMAILER_SMTP_LOGIN', 'slistem@slate.co.jp');
+
 $sAction = "ppasa";
 $sType = "not";
 $nPK = "0";
