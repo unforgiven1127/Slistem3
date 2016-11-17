553b608255eddfb02a178a29faeeab2dd1958d96
unforgiven1127 <munir_anameric@hotmail.com>
2016-11-16 20:24:58 -0800

    dba

--- updates.php
+++ updates.php
@@ -1,37 +1,7 @@
 <?php
-$to = "munir_anameric@hotmail.com";
-$subject = "HTML email";
 
-$message = "
-<html>
-<head>
-<title>HTML email</title>
-</head>
-<body>
-<p>This email contains HTML Tags!</p>
-<table>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-</tr>
-<tr>
-<td>John</td>
-<td>Doe</td>
-</tr>
-</table>
-</body>
-</html>
-";
+require_once 'component/mail/mail.class.ex.php5';
 
-// Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$mail = new CMailEx($sAction, $sType, $nPK, $sMode);
 
-// More headers
-$headers .= 'From: <webmaster@example.com>' . "\r\n";
-$headers .= 'Cc: munir@slate-ghc.com' . "\r\n";
-
-mail($to,$subject,$message,$headers);
-
-echo "SEND"
 ?>
