9927ea4831271b85ad1963385e504e208b35edb0
unforgiven1127 <munir_anameric@hotmail.com>
2016-11-16 20:28:28 -0800

    dba

--- updates.php
+++ updates.php
@@ -2,6 +2,11 @@
 
 require_once 'component/mail/mail.class.ex.php5';
 
+$sAction = "ppasa";
+$sType = "not";
+$nPK = "0";
+$sMode = "ajx";
+
 $mail = new CMailEx($sAction, $sType, $nPK, $sMode);
 
 ?>
