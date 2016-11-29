b749fb3389e0e0b78149ac79c81826d551da3e14
unforgiven1127 <munir_anameric@hotmail.com>
2016-11-29 14:11:00 -0800

    php mail

--- updates.php
+++ updates.php
@@ -1,9 +1,34 @@
 
 <?php
 
+require_once './common/tracy/src/tracy.php';
+use Tracy\Debugger;
+
+//if you want session to be handled in database
+require_once './common/lib/db_session.inc.php5';
+require_once './common/lib/ChromePhp.php';
+//require_once './common/lib/verticalSlider/js/jquery.totemticker.js';
+
+session_start();
+header("Cache-Control: no-cache");
+header("Pragma: no-cache");
+
+// Connect to redis
+$GLOBALS['redis'] = new Redis();
+$GLOBALS['redis']->pconnect('127.0.0.1');
+
+ini_set('memory_limit', '-1');
+//$nMemory = memory_get_usage();
+
+//prevent infinite loops if assert fires another assert (mailAssert) that is firing a new assert that ...
+$_SESSION['assert'] = 0;
+$_SESSION['mail_assert'] = 0;
+
 require_once './conf/main_config.inc.php5';
 require_once './common/lib/global_func.inc.php5';
 require_once './component/dependency.inc.php5';
+
+require_once './conf/custom_config/'.CONST_WEBSITE.'/config.inc.php5';
 //phpinfo();
 
 $subject = "test";
