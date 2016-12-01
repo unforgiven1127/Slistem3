06d18423452a00dfc89f9bed79b9def2f6917571
unforgiven1127 <munir_anameric@hotmail.com>
2016-11-30 16:42:17 -0800

    japanese resume create

--- meetingReport.php
+++ meetingReport.php
@@ -143,7 +143,7 @@ $objPHPExcel->setActiveSheetIndex(0)
 // Miscellaneous glyphs, UTF-8
 $objPHPExcel->setActiveSheetIndex(0)
             ->setCellValue('A4', 'Miscellaneous glyphs')
-            ->setCellValue('A5', 'éàèùâêîôûëïüÿäöüç');
+            ->setCellValue('A5', 'asdasasd');
 
 // Rename worksheet
 $objPHPExcel->getActiveSheet()->setTitle('Simple');
@@ -155,7 +155,7 @@ $objPHPExcel->setActiveSheetIndex(0);
 
 // Redirect output to a client’s web browser (Excel2007)
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-header('Content-Disposition: attachment;filename="01simple.xlsx"');
+header('Content-Disposition: attachment;filename="test.xlsx"');
 header('Cache-Control: max-age=0');
 // If you're serving to IE 9, then the following may be needed
 header('Cache-Control: max-age=1');
