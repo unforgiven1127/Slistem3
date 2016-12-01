572a680d2c8b4bb0dcb948baa3e2e7b620b98061
unforgiven1127 <munir_anameric@hotmail.com>
2016-11-30 16:32:26 -0800

    japanese resume create

--- meetingReport.php
+++ meetingReport.php
@@ -29,6 +29,16 @@ require_once './common/lib/global_func.inc.php5';
 require_once './component/dependency.inc.php5';
 
 require_once './conf/custom_config/'.CONST_WEBSITE.'/config.inc.php5';
+
+/** PHPExcel */
+//include 'PHPExcel.php';
+require_once('common/lib/PHPExcel/Classes/PHPExcel.php');
+
+/** PHPExcel_Writer_Excel2007 */
+//include 'PHPExcel/Writer/Excel2007.php';
+require_once('common/lib/PHPExcel/Classes/PHPExcel/Writer/Excel2007.php');
+
+
 //phpinfo();
 
 //imap_timeout(IMAP_OPENTIMEOUT, 10);//hata gitmis gorunuyor.
@@ -90,12 +100,50 @@ foreach ($result as $key => $value)
 	}
 }
 
-foreach ($meetingArray as $key => $value)
+/*foreach ($meetingArray as $key => $value)
 {
 	echo $key.' - ';
 	var_dump($value);
 	echo '<br><br>';
-}
+}*/
+
+/** Error reporting */
+error_reporting(E_ALL);
+
+
+
+// Create new PHPExcel object
+echo date('H:i:s') . " Create new PHPExcel object\n";
+$objPHPExcel = new PHPExcel();
+
+// Set properties
+echo date('H:i:s') . " Set properties\n";
+$objPHPExcel->getProperties()->setCreator("Maarten Balliauw");
+$objPHPExcel->getProperties()->setLastModifiedBy("Maarten Balliauw");
+$objPHPExcel->getProperties()->setTitle("Office 2007 XLSX Test Document");
+$objPHPExcel->getProperties()->setSubject("Office 2007 XLSX Test Document");
+$objPHPExcel->getProperties()->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.");
+
+
+// Add some data
+echo date('H:i:s') . " Add some data\n";
+$objPHPExcel->setActiveSheetIndex(0);
+$objPHPExcel->getActiveSheet()->SetCellValue('A1', 'Hello');
+$objPHPExcel->getActiveSheet()->SetCellValue('B2', 'world!');
+$objPHPExcel->getActiveSheet()->SetCellValue('C1', 'Hello');
+$objPHPExcel->getActiveSheet()->SetCellValue('D2', 'world!');
+
+// Rename sheet
+echo date('H:i:s') . " Rename sheet\n";
+$objPHPExcel->getActiveSheet()->setTitle('Simple');
+
+// Save Excel 2007 file
+echo date('H:i:s') . " Write to Excel2007 format\n";
+$objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel);
+$objWriter->save(str_replace('.php', '.xlsx', __FILE__));
+
+// Echo done
+echo date('H:i:s') . " Done writing file.\r\n";
 
 /*if( mail('munir_anameric@hotmail.com', 'test subject', 'hello this is a test') ){
 
