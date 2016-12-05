a502cc25f03866baeec2c38029e9bc6d54c38103
unforgiven1127 <munir_anameric@hotmail.com>
2016-12-04 16:20:44 -0800

    meeting report

--- sqlbackup.php
+++ sqlbackup.php
@@ -6,16 +6,14 @@
 require_once './common/lib/dropbox-sdk/lib/Dropbox/autoload.php';
 use \Dropbox as dbx;
 
-
 //your access token from the Dropbox App Panel
 $accessToken = 'ONKIZcLnUBAAAAAAAAAAB8z48X7hn2KiBDLh8HM7xfszzyuIdOovFX0Y5yKONq6K';
 echo 'Backup started '.date('Y-m-d h:i:s')."<br><br>";
 //run the MySQL dump and zip;
 
 // location of your temp directory
-$tmpDir = "/web/backup/";
+$tmpDir = __DIR__."/backup/";
 // username for MySQL
-
 $user = "pma_admin";
 // password for MySQL
 $password = "KVW4PVVAWHASuRDz";
@@ -27,26 +25,21 @@ $dbHost = "127.0.0.1";
 $prefix = "db_backup_";
 
 // Create the database backup file
-$sqlFile = $tmpDir.$prefix.date('Y_m_d').".sql";
-//$backupFilename = $prefix.".tgz";
-$backupFilename = $sqlFile.".tgz";
+$sqlFile = $tmpDir.$prefix.date('Y_m_d_').".sql";
+$backupFilename = $prefix.".tgz";
 $backupFile = $tmpDir.$backupFilename;
 
-$createBackup = "sudo mysqldump -h ".$dbHost." -u ".$user." -p ".$password." ".$dbName." > ".$sqlFile;
-echo $createBackup;
-echo '<br><br>';
-
-echo "mysql -u pma_admin -h 127.0.0.1 -p KVW4PVVAWHASuRDz";
-echo '<br><br>';
-
-//$createZip = "zip --password slateuppermainland $backupFile $sqlFile";
-$createZip = "sudo tar cvzf ".$backupFile." $sqlFile";
-echo $createZip;
+$createBackup = "mysqldump -h ".$dbHost." -u ".$user." --password='".$password."' ".$dbName." --> ".$sqlFile;
+//echo $createBackup;
 //echo '<br><br>';
-echo '<br><br>';
+//$createZip = "zip --password slateuppermainland $backupFile $sqlFile";
+$createZip = "tar cvzf $backupFile $sqlFile";
+//echo $createZip;
+//echo '<br><br>';
+//echo '<br><br>';
 exec($createBackup);
 exec($createZip);
-//echo $createBackup;
+
 //now run the DBox app info and set the client; we are naming the app folder SQL_Backup but CHANGE THAT TO YOUR ACTUAL APP FOLDER NAME;
 
 //$appInfo = dbx\AppInfo::loadFromJsonFile(__DIR__."/config.json");
@@ -67,17 +60,16 @@ exec($createZip);
 //unlink($sqlFile);
 //unlink($backupFile);
 
-$dNow = date('Y-m-d');
+//$dNow = date('Y-m-d');
 
-//$to      = 'ray@slate-ghc.com;mmoir@slate.co.jp;munir@slate-ghc.com';
-$to      = 'munir@slate-ghc.com';
+$to      = 'ray@slate-ghc.com;mmoir@slate-ghc.com;munir@slate-ghc.com';
 $subject = 'Backup completed';
 $message = "Slistem Backup '".$dNow."' completed";
 $headers = 'From: slistem@slate.co.jp' . "\r\n" .
     'Reply-To: munir@slate-ghc.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-mail($to, $subject, $message, $headers);
+//mail($to, $subject, $message, $headers);
 
 echo 'Backup uploaded successfully'.date('Y-m-d h:i:s');
 
