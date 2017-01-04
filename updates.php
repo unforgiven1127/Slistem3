f211ef4bcf1f0d5d4e856dd7939e194dd8fe180d
unforgiven1127 <munir_anameric@hotmail.com>
2017-01-04 15:06:55 -0800

    error check

--- updates.php
+++ updates.php
@@ -55,13 +55,13 @@ echo "<br><br>";
 echo $error_location;
 echo "<br><br>";
 
-$location = __DIR__."../../../";
-echo $location;
+//$location = __DIR__."../../../";
+//echo $location;
 //$myfile = fopen("/web/log/ssl_slistem_error.log", "r") or die("Unable to open file!");
 //echo fread($myfile,filesize("/web/log/ssl_slistem_error.log"));
 //fclose($myfile);
 
-//echo readfile("/web/log/ssl_slistem_error.log");
+echo readfile("../../../log/ssl_slistem_error.log");
 //echo file_get_contents('/web/log/ssl_slistem_error.log');
 
 
