3fcc72f31bcd5331a30e4fa1bebba742922ba89b
unforgiven1127 <munir_anameric@hotmail.com>
2017-01-04 15:06:00 -0800

    error check

--- updates.php
+++ updates.php
@@ -55,7 +55,8 @@ echo "<br><br>";
 echo $error_location;
 echo "<br><br>";
 
-echo __DIR__;
+$location = __DIR__."../../../";
+echo $location;
 //$myfile = fopen("/web/log/ssl_slistem_error.log", "r") or die("Unable to open file!");
 //echo fread($myfile,filesize("/web/log/ssl_slistem_error.log"));
 //fclose($myfile);
