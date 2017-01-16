0732dcfb3672036a22d01a68dc0d9cbb6b937eee
unforgiven1127 <munir_anameric@hotmail.com>
2017-01-15 20:25:27 -0800

    mongo db

--- updates.php
+++ updates.php
@@ -30,7 +30,7 @@ require_once './component/dependency.inc.php5';
 
 require_once './conf/custom_config/'.CONST_WEBSITE.'/config.inc.php5';
 
-echo "test";
+echo "test<br><br>";
 
 $connection = new MongoClient();
 $collection = $connection->database->collectionName;
