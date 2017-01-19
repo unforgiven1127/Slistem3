782c079627df10977908662c6e54d10cebb78566
unforgiven1127 <munir_anameric@hotmail.com>
2017-01-18 21:46:19 -0800

    status control

--- updates.php
+++ updates.php
@@ -86,6 +86,7 @@ $allLogs = $logsSlistemMongo->find();
 foreach($allLogs as $log)
 {
     echo "<br><br>";
+    echo "user: ".$log['userfk']."<br><br>";
     echo "candidate_id: ".$log['cp_pk']."<br><br>";
     echo "date: ".$log['date']."<br><br>";
     echo "table: ".$log['table']."<br><br>";
