037207057d6ab768ae1183d442b59404e8d6ea2e
unforgiven1127 <munir_anameric@hotmail.com>
2017-01-18 21:53:56 -0800

    status control

--- updates.php
+++ updates.php
@@ -79,7 +79,7 @@ $newLog = array(
 
 //$where = array('cp_pk' => '154310','table' => 'user_history_all_view');
 $where = array('$or' => array(
-  array("cp_pk" => "154310"),
+  array("userfk" => "101"),
   array("table" => "user_history_all_view")
 ));
 $orderBy = array('date' => -1);//(1 : ASC , -1 : DESC)
