637734143ebbc7e7d49b5fded20707fec4f32432
unforgiven1127 <munir_anameric@hotmail.com>
2017-02-23 20:16:24 -0800

    candidate mail

--- component/display/resources/html/candidate_mail_send.php
+++ component/display/resources/html/candidate_mail_send.php
@@ -1,8 +1,20 @@
+<style>
+	div.formFieldTitle
+	{
+	  background-color: #efefef;
+	  border: 1px solid #e0e0e0;
+	  color: #077AC1;
+	  font-size: 13px;
+	  margin: 5px 0;
+	  padding: 5px;
+	}
+</style>
+
 <form>
 	<table style="width:100%;" valign="top">
 		<tr>
 			<td style="width:100%;" valign="top">
-				<div inajax="" id="58afb3d10beb2Id" class=" formFieldTitle">Send a message</div>
+				<div inajax="" id="58afb3d10beb2Id" class="formFieldTitle">Send a message</div>
 			</td>
 		</tr>
 	</table>
