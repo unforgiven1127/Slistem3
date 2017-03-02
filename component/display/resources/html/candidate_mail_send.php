432a63419dd926503e2e7d4b651fd8a89efb08f4
unforgiven1127 <munir_anameric@hotmail.com>
2017-03-02 14:00:40 -0800

    candidate mail

--- component/display/resources/html/candidate_mail_send.php
+++ component/display/resources/html/candidate_mail_send.php
@@ -9,6 +9,7 @@ $( "#sendMailToCandidate" ).click(function() {
 
 	function addNewDocumentSelection()
 	{
+		alert('test');
 	    var x = document.createElement("INPUT");
 	    x.setAttribute("type", "file");
 	    document.body.appendChild(x);
