b297b0cdc97e80d683505f6a25a3847c414e5c1f
unforgiven1127 <munir_anameric@hotmail.com>
2017-03-02 20:58:53 -0800

    candidate mail

--- component/display/resources/html/candidate_mail_send.php
+++ component/display/resources/html/candidate_mail_send.php
@@ -1,6 +1,3 @@
-
-  <script src="//cloud.tinymce.com/stable/tinymce.min.js"></script>
-  <script>tinymce.init({ selector:'textarea' });</script>
 <script>
 
 function loading()
@@ -143,7 +140,14 @@ $( "#sendMailToCandidate" ).click(function() {
 		<tr>
 			<td valign="top" style='padding-top:15px; padding-left: 50px; width:70px;'><b>Message: </b></td>
 			<td style='padding-top:15px;' align="left">
-				<textarea style="outline: none; width: 500px; height: 300px;" name="message" class="msgField" inajax="" id="messageId" name="messageId"><?php echo $signature; ?></textarea>
+				<textarea style="outline: none; width: 500px; height: 300px;" name="message" class="msgField" inajax="" id="messageId" name="messageId"></textarea>
+			</td>
+		</tr>
+	</table>
+	<table>
+		<tr>
+			<td style='padding-top:15px;' align="left">
+				<textarea style="outline: none; width: 500px; height: 300px;" name="signature" class="msgField" inajax="" id="signature"><?php echo $signature; ?></textarea>
 			</td>
 		</tr>
 	</table>
@@ -172,56 +176,6 @@ $( "#sendMailToCandidate" ).click(function() {
 				<center>
 					<input name="Send" type="submit" id="sendMailToCandidate" value="Send" onclick="">
 				</center>
-			</td>
-		</tr>
-	</table>
-	<table>
-		<tr>
-			<td>
-				<textarea><p style="text-align: center; font-size: 15px;"><img title="TinyMCE Logo" src="//www.tinymce.com/images/glyph-tinymce@2x.png" alt="TinyMCE Logo" width="110" height="97" />
-  </p>
-  <h1 style="text-align: center;">Welcome to the TinyMCE & Community Plugins demo!</h1>
-  <h5 style="text-align: center;">Note, this is not an "enterprise/premium" demo.<br>Visit the <a href="https://www.tinymce.com/pricing/#demo-enterprise">pricing page</a> to demo our premium plugins.</h5>
-  <p>Please try out the features provided in this full featured example.</p>
-  <p>Note that any <b>MoxieManager</b> file and image management functionality in this example is part of our commercial offering – the demo is to show the integration.</h2>
-
-  <h2>Got questions or need help?</h2>
-  <ul>
-    <li>Our <a href="//www.tinymce.com/docs/">documentation</a> is a great resource for learning how to configure TinyMCE.</li>
-    <li>Have a specific question? Visit the <a href="http://community.tinymce.com/forum/">Community Forum</a>.</li>
-    <li>We also offer enterprise grade support as part of <a href="http://tinymce.com/pricing">TinyMCE Enterprise</a>.</li>
-  </ul>
-
-  <h2>A simple table to play with</h2>
-  <table style="text-align: center;">
-    <thead>
-      <tr>
-        <th>Product</th>
-        <th>Cost</th>
-        <th>Really?</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>TinyMCE</td>
-        <td>Free</td>
-        <td>YES!</td>
-      </tr>
-      <tr>
-        <td>Plupload</td>
-        <td>Free</td>
-        <td>YES!</td>
-      </tr>
-    </tbody>
-  </table>
-
-  <h2>Found a bug?</h2>
-  <p>If you think you have found a bug please create an issue on the <a href="https://github.com/tinymce/tinymce/issues">GitHub repo</a> to report it to the developers.</p>
-
-  <h2>Finally ...</h2>
-  <p>Don't forget to check out our other product <a href="http://www.plupload.com" target="_blank">Plupload</a>, your ultimate upload solution featuring HTML5 upload support.</p>
-  <p>Thanks for supporting TinyMCE! We hope it helps you and your users create great content.<br>All the best from the TinyMCE team.</p>
-</textarea>
 			</td>
 		</tr>
 	</table>
