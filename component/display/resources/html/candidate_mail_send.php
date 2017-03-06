fe9f0a047d6302480d29bf71337b0e025f40c08b
unforgiven1127 <munir_anameric@hotmail.com>
2017-03-05 21:02:27 -0800

    candidate mail

--- component/display/resources/html/candidate_mail_send.php
+++ component/display/resources/html/candidate_mail_send.php
@@ -14,22 +14,20 @@ $( "#sendMailToCandidate" ).click(function() {
     validation = true;
 alert(cc.length);
 alert(bcc.length);
-    if(message.length == 0 || subject.length == 0 || (cc.length > 0 && cc.indexOf("@") < 0) || (bcc.length > 0 && bcc.indexOf("@") < 0))
+    if(message.length == 0 || subject.length == 0)
     {
+    	alert('Subject & Message should be filled.');
     	$validation = false;
     }
-    else
+    if((cc.length > 0 && cc.indexOf("@") < 0) || (bcc.length > 0 && bcc.indexOf("@") < 0))
     {
-    	validation = true;
+		alert('CC & BCC should be valid.');
+    	$validation = false;
     }
 
 	if ($validation) {
 	    // submit with AJAX
 	    loading();
-	}
-	else
-	{
-		alert('Validation error!!');
 	}
 
     /*var receipent_email = $('#receipent_email').val();
