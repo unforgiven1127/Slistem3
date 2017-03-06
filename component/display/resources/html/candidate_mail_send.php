9db1c03591373c9c1978b84a48123900d5ec0871
unforgiven1127 <munir_anameric@hotmail.com>
2017-03-05 20:50:38 -0800

    candidate mail

--- component/display/resources/html/candidate_mail_send.php
+++ component/display/resources/html/candidate_mail_send.php
@@ -12,7 +12,7 @@ $( "#sendMailToCandidate" ).click(function() {
 	var cc = $('#cc').val();
     var bcc = $('#bcc').val();
 alert(cc);
-    if(message == "" || subject == "" || (cc != "" && cc.indexOf("@") < 0) || (bcc != "" && bcc.indexOf("@") < 0))
+    if(message == "" || subject == "" || (cc.length > 0 && cc.indexOf("@") < 0) || (bcc.length > 0 && bcc.indexOf("@") < 0))
     {
     	$validation = false;
     }
