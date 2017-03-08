3c01902014e4940586e05cf74c9445080ce02cda
unforgiven1127 <munir_anameric@hotmail.com>
2017-03-07 20:29:17 -0800

    candidate mail

--- component/display/resources/html/candidate_mail_send.php
+++ component/display/resources/html/candidate_mail_send.php
@@ -4,9 +4,32 @@
     'image'
   ], });</script>-->
 <script type="text/javascript" >
+	CKEDITOR.editorConfig = function( config )
+	{
+		config.toolbar = 'MyToolbar';
+
+		config.toolbar_MyToolbar =
+		[
+			{ name: 'document', items : [ 'NewPage','Preview' ] },
+			{ name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+			{ name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','Scayt' ] },
+			{ name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'
+	                 ,'Iframe' ] },
+	                '/',
+			{ name: 'styles', items : [ 'Styles','Format' ] },
+			{ name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
+			{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
+			{ name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+			{ name: 'tools', items : [ 'Maximize','-','About' ] }
+		];
+	};
                 // Replace the <textarea id="editor1"> with a CKEditor
                 // instance, using default configuration.
-    CKEDITOR.replace( 'message' );
+    CKEDITOR.replace( 'message',
+	{
+		toolbar : 'MyToolbar'
+	});
+
 </script>
 
 
