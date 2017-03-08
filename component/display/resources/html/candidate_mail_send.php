c8053830641d9aee0e54d7a6eb6bc2cb351bc287
unforgiven1127 <munir_anameric@hotmail.com>
2017-03-07 20:30:49 -0800

    candidate mail

--- component/display/resources/html/candidate_mail_send.php
+++ component/display/resources/html/candidate_mail_send.php
@@ -10,17 +10,10 @@
 
 		config.toolbar_MyToolbar =
 		[
-			{ name: 'document', items : [ 'NewPage','Preview' ] },
-			{ name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
-			{ name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','Scayt' ] },
-			{ name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'
-	                 ,'Iframe' ] },
-	                '/',
+			{ name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak' ] },
 			{ name: 'styles', items : [ 'Styles','Format' ] },
 			{ name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
-			{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
-			{ name: 'links', items : [ 'Link','Unlink','Anchor' ] },
-			{ name: 'tools', items : [ 'Maximize','-','About' ] }
+			{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] }
 		];
 	};
                 // Replace the <textarea id="editor1"> with a CKEditor
