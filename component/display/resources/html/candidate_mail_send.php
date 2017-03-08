493a963c29600b9eb2dfd37724fd49e1a5c6ad0d
unforgiven1127 <munir_anameric@hotmail.com>
2017-03-07 19:09:34 -0800

    candidate mail

--- component/display/resources/html/candidate_mail_send.php
+++ component/display/resources/html/candidate_mail_send.php
@@ -4,7 +4,7 @@
     'image'
   ], });</script>-->
 
-<script type="text/javascript" src="http://js.nicedit.com/nicEdit-latest.js"></script> 
+
 <script type="text/javascript">
 //<![CDATA[
         bkLib.onDomLoaded(function() { nicEditors.allTextAreas() });
