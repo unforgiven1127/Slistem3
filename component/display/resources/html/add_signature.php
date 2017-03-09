f5613e9bbd2581ae8087c73d4b3c320dc1c16b7c
unforgiven1127 <munir_anameric@hotmail.com>
2017-03-08 19:57:00 -0800

    candidate mail

--- component/display/resources/html/add_signature.php
+++ component/display/resources/html/add_signature.php
@@ -6,7 +6,7 @@
 <script type="text/javascript" >
                 // Replace the <textarea id="editor1"> with a CKEditor
                 // instance, using default configuration.
-    CKEDITOR.replace( 'fmsg',
+    CKEDITOR.replace( 'message',
 	{
 		filebrowserBrowseUrl : '/browser/browse.php',
 		filebrowserUploadUrl : '/uploader/upload.php'
