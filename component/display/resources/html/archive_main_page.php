03d77120e4843840d59d0c8fd95bef182c3c258c
unforgiven1127 <munir_anameric@hotmail.com>
2017-03-21 19:00:29 -0700

    historical company list

--- component/display/resources/html/archive_main_page.php
+++ component/display/resources/html/archive_main_page.php
@@ -7,11 +7,6 @@
 
 </script>
 
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 <style>
 
@@ -38,7 +33,8 @@
 					</td>
 				</tr>
 			</table>
-			<table class="table table-striped">
+			<table id='activeUsers' class="table table-striped">
+
 					<?php foreach ($positions as $key => $position)
 					{
 						if($position['status'] == 1)//active users
