9c8bfe6a1772b70e0f396f7f60019c1e08627df1
unforgiven1127 <munir_anameric@hotmail.com>
2017-03-21 19:54:39 -0700

    position user

--- component/display/resources/html/archive_main_page.php
+++ component/display/resources/html/archive_main_page.php
@@ -59,7 +59,7 @@
 							echo "<td class='userInfo'>-</td>";
 							echo "</tr>";
 							echo "<tr>";
-							echo "<td colspan='2'>";
+							echo "<td colspan='3'>";
 							echo "<table  class='user_".$position['user_id']." table table-striped' style='width:100%;'>";
 							foreach ($position['position_id'] as $key => $value)
 							{
