3ca6cc9bb63793d454371595bc032c192238c752
unforgiven1127 <munir_anameric@hotmail.com>
2017-03-21 21:02:21 -0700

    position user

--- component/display/resources/html/archive_main_page.php
+++ component/display/resources/html/archive_main_page.php
@@ -67,7 +67,7 @@
 					<th>
 						<p style='font-size: 14pt; font-weight: bold;'>Active Users</p>
 					</th>
-					<td style='padding-left: 100px !important;'><img style='cursor:pointer; width: 20px; vertical-align: text-bottom;' src='common/pictures/plus.png' onclick='openActiveUsers('activeUsers')'> <img style='cursor:pointer; width: 20px; vertical-align: text-bottom;' src='common/pictures/minus.png' onclick='closeActiveUsers('activeUsers')'></td>
+					<td style='padding-left: 110px !important;'><img style='cursor:pointer; width: 20px; vertical-align: text-bottom;' src='common/pictures/plus.png' onclick='openActiveUsers('activeUsers')'> <img style='cursor:pointer; width: 20px; vertical-align: text-bottom;' src='common/pictures/minus.png' onclick='closeActiveUsers('activeUsers')'></td>
 				</tr>
 			</table>
 			<table style='width:100%;' id='activeUsers' class="table table-striped'">
