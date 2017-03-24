78ec0392be4ef8629dab13aff82348c2b5732f5a
unforgiven1127 <munir_anameric@hotmail.com>
2017-03-23 20:40:23 -0700

    position user

--- component/display/resources/html/company_position_list.php
+++ component/display/resources/html/company_position_list.php
@@ -1,13 +1,10 @@
 <style>
-    .table
+    .tableS
     {
         border-collapse: collapse !important;
     }
-    .striped1
-    {
-        background-color: #f9f9f9;
-    }
-    .header
+
+    .headerS
     {
         padding-left: 5px;
         color:white;
@@ -33,13 +30,13 @@
 <!DOCTYPE html>
 <html>
 <body>
-<table style='width:1000px; margin-top: 10px;' id='activeUsers' class="table table-striped1">
+<table style='width:1000px; margin-top: 10px;' id='activeUsers' class="tableS">
     <tr style="background-color: #446184;" >
-        <td class='header'>Position ID</td>
-        <td class='header'>Position Name</td>
-        <td class='header'>Last Status</td>
-        <td class='header'>Created By</td>
-        <td class='header'>Create Date</td>
+        <td class='headerS'>Position ID</td>
+        <td class='headerS'>Position Name</td>
+        <td class='headerS'>Last Status</td>
+        <td class='headerS'>Created By</td>
+        <td class='headerS'>Create Date</td>
     </tr>
 </table>
 </body>
