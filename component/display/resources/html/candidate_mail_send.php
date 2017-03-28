b872fd36a02a030b53ab6df91c253e0c355de926
unforgiven1127 <munir_anameric@hotmail.com>
2017-03-27 20:27:45 -0700

    mail send

--- component/display/resources/html/candidate_mail_send.php
+++ component/display/resources/html/candidate_mail_send.php
@@ -148,7 +148,7 @@ $( "#sendMailToCandidate1" ).click(function() {
 
       $('#sendMailToCandidate').click(function(){
       	alert('test');
-        //loadingNew();
+        loadingNew();
         var dataToUpload = new Array();
         dataToUpload.formData = $('.single-upload-form').serializeArray();
         dataToUpload.files = fileToUpload;
@@ -268,7 +268,7 @@ $( "#sendMailToCandidate1" ).click(function() {
 		<tr>
 			<td>
 				<center>
-					<input class='submitBtnClass' name="Send" type="button" id="sendMailToCandidate" value="Send" onclick="">
+					<input class='submitBtnClass' name="Send" type="submit" id="sendMailToCandidate" value="Send" onclick="">
 				</center>
 			</td>
 		</tr>
