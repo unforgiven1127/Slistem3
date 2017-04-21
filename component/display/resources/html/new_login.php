b11d368433f4211ace981eae6f0026de5c69d468
unforgiven1127 <munir_anameric@hotmail.com>
2017-04-20 20:53:36 -0700

    6box new design

--- component/display/resources/html/new_login.php
+++ component/display/resources/html/new_login.php
@@ -92,7 +92,7 @@ function deleteAllCookies() {
 function loading()
 {
     localStorage.setItem("loginFlag", "loginPage");
-    //deleteAllCookies();
+    deleteAllCookies();
     $('body').addClass('noScroll').append('<div id="slLoadingScreen"  style="z-index: 999; width: '+ ($(document).innerWidth() + 100) +'px; height: '+ ($(document).innerHeight() + 100) +'px; position: absolute; top: 0; left: 0; ">      <div class="bg"></div><div class="ani"></div></div>');
     $('body').append("<div id='overlay' class='overlay'></div>");
 }
