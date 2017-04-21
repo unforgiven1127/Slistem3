65c5e8eae3a587a6a4f431d2e318e6c9a41fa4c6
unforgiven1127 <munir_anameric@hotmail.com>
2017-04-20 20:44:12 -0700

    6box new design

--- component/display/resources/html/new_login.php
+++ component/display/resources/html/new_login.php
@@ -84,13 +84,8 @@ function openExtra(open)
 
 function deleteAllCookies() {
     var cookies = document.cookie.split(";");
-
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
+    for (var i = 0; i < cookies.length; i++)
+      eraseCookie(cookies[i].split("=")[0]);
 }
 
 function loading()
