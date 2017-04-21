21ab94d53c6ba0593d83ee623dc7ca3828e236ec
unforgiven1127 <munir_anameric@hotmail.com>
2017-04-20 20:37:21 -0700

    6box new design

--- component/display/resources/html/new_login.php
+++ component/display/resources/html/new_login.php
@@ -81,9 +81,22 @@ function openExtra(open)
     document.getElementById(open).style.display = "table-row";
 }
 
+
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
 function loading()
 {
     localStorage.setItem("loginFlag", "loginPage");
+    deleteAllCookies();
     $('body').addClass('noScroll').append('<div id="slLoadingScreen"  style="z-index: 999; width: '+ ($(document).innerWidth() + 100) +'px; height: '+ ($(document).innerHeight() + 100) +'px; position: absolute; top: 0; left: 0; ">      <div class="bg"></div><div class="ani"></div></div>');
     $('body').append("<div id='overlay' class='overlay'></div>");
 }
