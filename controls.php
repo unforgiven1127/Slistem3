bf3a726a6b7bfc23df59636c507336398227a373
unforgiven1127 <munir_anameric@hotmail.com>
2017-04-23 17:45:39 -0700

    mysql

--- controls.php
+++ controls.php
@@ -40,7 +40,7 @@ define('DB_PASSWORD_SLISTEM', 'KVW4PVVAWHASuRDz');
 
 //echo phpinfo();
 
-mysqli_connect( DB_SERVER_SLISTEM, DB_USER_SLISTEM, DB_PASSWORD_SLISTEM) or die(mysql_error());
+mysql_connect( DB_SERVER_SLISTEM, DB_USER_SLISTEM, DB_PASSWORD_SLISTEM) or die(mysql_error());
 mysql_select_db(DB_NAME_SLISTEM) or die(mysql_error());
 
 
