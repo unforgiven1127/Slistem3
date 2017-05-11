d4ebe842dea21dab2a5f4ab3c029de5a212f66b5
unforgiven1127 <munir_anameric@hotmail.com>
2017-05-10 20:24:25 -0700

    is client

--- component/display/resources/html/candidate_add.php
+++ component/display/resources/html/candidate_add.php
@@ -223,7 +223,7 @@
 						<input type="text" name="keyword" value="<?php echo $keyword; ?>" />
 					</div>
 					<div class="general_form_label add_margin_left_30">Is client: </div>
-					<div><?php echo $isClient; ?></div>
+					<div class="general_form_column" ><?php echo $isClient; ?></div>
 					<!--<div class="general_form_column">
 						<input id="is_client" class="css-checkbox" type="checkbox" name="client"
 							<?php if (!empty($is_client)) echo 'checked'; ?> />
