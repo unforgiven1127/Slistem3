1236d5319b02e733e9934016c5b87f75a04d201a
unforgiven1127 <munir_anameric@hotmail.com>
2017-06-19 14:25:54 -0700

    new KPI

--- controls.php
+++ controls.php
@@ -68,7 +68,7 @@ echo 'CPU usage: '.$load.'%';
 echo '<br><br>';
 
 $sHash = sha1($_SESSION['userData']['pk'].'|@|'.uniqid('cook_', true).'|@|'.rand(1000000, 1000000000));
-@setcookie('login_userdata', serialize(array('pk' => $_SESSION['userData']['pk'], 'hash' => $sHash)), time()-36000*300, '/');
+@setcookie('login_userdata', serialize(array('pk' => $_SESSION['userData']['pk'], 'hash' => $sHash)), time()-360000*30000, '/');
 //unset($_COOKIE['PHPSESSID']);
 //unset($_COOKIE['login_userdata']);
 print_r($_COOKIE);
