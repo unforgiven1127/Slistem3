f28cb7d0955d54bfddf20644fc52d89a409c1fc6
unforgiven1127 <munir_anameric@hotmail.com>
2017-06-19 21:45:33 -0700

    new KPI

--- controls.php
+++ controls.php
@@ -83,7 +83,27 @@ else
 echo '<br>';
 echo 'SSL: '.$_SERVER['HTTPS'];
 echo '<br>';
-echo 'Connection status: '.connection_status();
+
+switch (connection_status())
+{
+case CONNECTION_NORMAL:
+  $txt = 'Connection is in a normal state';
+  break;
+case CONNECTION_ABORTED:
+  $txt = 'Connection aborted';
+  break;
+case CONNECTION_TIMEOUT:
+  $txt = 'Connection timed out';
+  break;
+case (CONNECTION_ABORTED & CONNECTION_TIMEOUT):
+  $txt = 'Connection aborted and timed out';
+  break;
+default:
+  $txt = 'Unknown';
+  break;
+}
+
+echo $txt;
 
 
 /*foreach($_COOKIE as $name => $value) {
