48cadcf7805a1787de524929b5492417e8637e3f
unforgiven1127 <munir_anameric@hotmail.com>
2017-07-25 15:27:41 -0700

    angular

--- component/display/resources/html/position_archive_angular.php
+++ component/display/resources/html/position_archive_angular.php
@@ -5,6 +5,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
   </head>
   <body>
+   <?php var_dump($users); ?>
   	<div ng-init="names = <?php echo $users ?>">
 
 		<table>
