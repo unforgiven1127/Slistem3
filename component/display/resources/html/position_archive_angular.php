edf39ed49ec94dbcae789f1a4b65025adde74fe1
unforgiven1127 <munir_anameric@hotmail.com>
2017-07-25 15:02:09 -0700

    angular

--- component/display/resources/html/position_archive_angular.php
+++ component/display/resources/html/position_archive_angular.php
@@ -1,10 +1,27 @@
 <!doctype html>
 <html ng-app>
   <head>
-    <title>My AngularJS App</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>
+    <title>Angular Position List</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
   </head>
   <body>
-  <div>test</div>
+  	<div ng-app="myApp" ng-controller="customersCtrl">
+
+		<table>
+		  <tr ng-repeat="x in names">
+		    <td>{{ x.position_name }}</td>
+		    <td>{{ x.company_name }}</td>
+		  </tr>
+		</table>
+	</div>
+
+	<script>
+	var app = angular.module('myApp', []);
+	$scope.names = $positions;
+	/*app.controller('customersCtrl', function($scope, $http) {
+	   $http.get("customers_mysql.php")
+	   .then(function (response) {$scope.names = response.data.records;});
+	});*/
+	</script>
   </body>
 </html>
