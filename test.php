bdeacfcd4e0abfe59154fedee723abcc5cf9968b
unforgiven1127 <munir_anameric@hotmail.com>
2017-07-25 18:49:36 -0700

    angular

--- test.php
+++ test.php
@@ -1,9 +1,20 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
 
+$conn = new mysqli("myServer", "myUser", "myPassword", "Northwind");
 
-header('Content-Type: application/json');
+$result = $conn->query("SELECT CompanyName, City, Country FROM Customers");
 
-$myObj = '{"name":"John","age":30,"city":"New York"}';
+$outp = "";
+while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+    if ($outp != "") {$outp .= ",";}
+    $outp .= '{"Name":"'  . $rs["CompanyName"] . '",';
+    $outp .= '"City":"'   . $rs["City"]        . '",';
+    $outp .= '"Country":"'. $rs["Country"]     . '"}';
+}
+$outp ='{"records":['.$outp.']}';
+$conn->close();
 
-echo $myObj;
-
+echo($outp);
+?>
