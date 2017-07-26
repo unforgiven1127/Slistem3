60db17ae85feb3c9ce9651dd7b7d6d034961d5bf
unforgiven1127 <munir_anameric@hotmail.com>
2017-07-25 18:54:19 -0700

    angular

--- component/display/resources/html/position_archive_angular.php
+++ component/display/resources/html/position_archive_angular.php
@@ -1,20 +1,11 @@
 <!DOCTYPE html>
 <html >
-<style>
-table, th , td  {
-  border: 1px solid grey;
-  border-collapse: collapse;
-  padding: 5px;
-}
-table tr:nth-child(odd) {
-  background-color: #f1f1f1;
-}
-table tr:nth-child(even) {
-  background-color: #ffffff;
-}
-</style>
+
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
 <body>
+
+
+
 
 <div ng-app="myApp" ng-controller="customersCtrl">
 
