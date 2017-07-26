d742af616d506d30d4ec52d7f57f324871093a46
unforgiven1127 <munir_anameric@hotmail.com>
2017-07-25 19:26:09 -0700

    angular

--- component/display/resources/html/position_archive_angular.php
+++ component/display/resources/html/position_archive_angular.php
@@ -4,6 +4,41 @@
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
 <body ng-app="myApp">
 
+<div class="container">
+         <div id="loginbox" class="mainbox col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3">
+            <div class="row">
+               <img src="images/logo.jpg" class="imglogo"/>
+            </div>
+            <div class="panel panel-default" >
+               <div class="panel-heading">
+                  <div class="panel-title text-center">Login using username & password</div>
+               </div>
+               <div class="panel-body" >
+                  <form name="login" ng-submit="postCtrl.postForm()" class="form-horizontal" method="POST">
+                     <div class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                        <input type="text" id="inputUsername" class="form-control" required autofocus ng-model="postCtrl.inputData.username"/>
+                     </div>
+                     <div class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                        <input type="password" id="inputPassword" class="form-control" required ng-model="postCtrl.inputData.password"/>
+                     </div>
+                     <div class="alert alert-danger" ng-show="errorMsg">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+                        ×</button>
+                        <span class="glyphicon glyphicon-hand-right"></span>&nbsp;&nbsp;{{errorMsg}}
+                     </div>
+                     <div class="form-group">
+                        <div class="col-sm-12 controls">
+                           <button type="submit" class="btn btn-primary pull-right" ng-disabled="login.$invalid">
+                           <i class="glyphicon glyphicon-log-in"></i> Log in</button>
+                        </div>
+                     </div>
+                  </form>
+               </div>
+            </div>
+         </div>
+      </div>
 
 <!-- Angular with json data htmlspecialchars part can be done at controller -->
 <div ng-init="positions = <?php echo htmlspecialchars(json_encode($test)); ?>">
@@ -21,7 +56,7 @@
 
 
 <!-- Angular with ajax style get -->
-<div  ng-controller="customersCtrl">
+<div ng-controller="customersCtrl">
 	<table style="margin-top: 10px;">
 	  <tr>
 	  	<th>Name</th>
@@ -35,11 +70,11 @@
 </div>
 
 <script>
-var app = angular.module('myApp', []);
-app.controller('customersCtrl', function($scope, $http) {
-   $http.get("test.php?f=test1")
-   .then(function (response) {$scope.names = response.data.records;});
-});
+	var app = angular.module('myApp', []);
+	app.controller('customersCtrl', function($scope, $http) {
+	   $http.get("test.php?f=test1")
+	   .then(function (response) {$scope.names = response.data.records;});
+	});
 </script>
 
 </body>
--- test.php
+++ test.php
@@ -9,29 +9,29 @@ if(isset($_GET['f']))
 
 function test1()
 {
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+	header("Access-Control-Allow-Origin: *");
+	header("Content-Type: application/json; charset=UTF-8");
 
-$rs = array();
-$add1 = array("CompanyName" => "abc company" , "Country" => "Canada" , "City" => "Rupert");
-$add2 = array("CompanyName" => "xyz company" , "Country" => "Turkey" , "City" => "Ankara");
-$add3 = array("CompanyName" => "tyu company" , "Country" => "Brazil" , "City" => "Rio");
+	$rs = array();
+	$add1 = array("CompanyName" => "abc company" , "Country" => "Canada" , "City" => "Rupert");
+	$add2 = array("CompanyName" => "xyz company" , "Country" => "Turkey" , "City" => "Ankara");
+	$add3 = array("CompanyName" => "tyu company" , "Country" => "Brazil" , "City" => "Rio");
 
-$rs[] = $add1;
-$rs[] = $add2;
-$rs[] = $add3;
+	$rs[] = $add1;
+	$rs[] = $add2;
+	$rs[] = $add3;
 
-$outp = "";
-foreach ($rs as $key => $value)
-{
-    if ($outp != "") {$outp .= ",";}
-    $outp .= '{"Name":"'  . $value["CompanyName"] . '",';
-    $outp .= '"City":"'   . $value["City"]        . '",';
-    $outp .= '"Country":"'. $value["Country"]     . '"}';
-}
-$outp ='{"records":['.$outp.']}';
+	$outp = "";
+	foreach ($rs as $key => $value)
+	{
+	    if ($outp != "") {$outp .= ",";}
+	    $outp .= '{"Name":"'  . $value["CompanyName"] . '",';
+	    $outp .= '"City":"'   . $value["City"]        . '",';
+	    $outp .= '"Country":"'. $value["Country"]     . '"}';
+	}
+	$outp ='{"records":['.$outp.']}';
 
-echo($outp);
+	echo($outp);
 }
 
 ?>
