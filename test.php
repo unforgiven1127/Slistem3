76d9bae235feb5ac941ab8fc34854573ab3272ce
unforgiven1127 <munir_anameric@hotmail.com>
2017-07-25 18:53:02 -0700

    angular

--- test.php
+++ test.php
@@ -2,19 +2,24 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-$conn = new mysqli("myServer", "myUser", "myPassword", "Northwind");
+$rs = array();
+$add1 = array("CompanyName" => "abc company" , "Country" => "Canada" , "City" => "Rupert");
+$add2 = array("CompanyName" => "xyz company" , "Country" => "Turkey" , "City" => "Ankara");
+$add3 = array("CompanyName" => "tyu company" , "Country" => "Brazil" , "City" => "Rio");
 
-$result = $conn->query("SELECT CompanyName, City, Country FROM Customers");
+$rs[] = $add1;
+$rs[] = $add2;
+$rs[] = $add3;
 
 $outp = "";
-while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+foreach ($rs as $key => $value)
+{
     if ($outp != "") {$outp .= ",";}
-    $outp .= '{"Name":"'  . $rs["CompanyName"] . '",';
-    $outp .= '"City":"'   . $rs["City"]        . '",';
-    $outp .= '"Country":"'. $rs["Country"]     . '"}';
+    $outp .= '{"Name":"'  . $value["CompanyName"] . '",';
+    $outp .= '"City":"'   . $value["City"]        . '",';
+    $outp .= '"Country":"'. $value["Country"]     . '"}';
 }
 $outp ='{"records":['.$outp.']}';
-$conn->close();
 
 echo($outp);
 ?>
