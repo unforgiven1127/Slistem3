0ae2f477c5acc38411224230237a8dd1fff0c21a
unforgiven1127 <munir_anameric@hotmail.com>
2017-07-25 18:13:05 -0700

    angular

--- test.php
+++ test.php
@@ -34,7 +34,7 @@ define('DB_PASSWORD_SLISTEM', 'KVW4PVVAWHASuRDz');
 
 
 
-mysql_connect( DB_SERVER_SLISTEM, DB_USER_SLISTEM, DB_PASSWORD_SLISTEM) or die(mysql_error());
+mysqli_connect( DB_SERVER_SLISTEM, DB_USER_SLISTEM, DB_PASSWORD_SLISTEM) or die(mysql_error());
 mysql_select_db(DB_NAME_SLISTEM) or die(mysql_error());
 
 
