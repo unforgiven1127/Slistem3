454dd3a37f67b64c47fc089332bc41feb5f6a15f
unforgiven1127 <munir_anameric@hotmail.com>
2017-08-02 18:55:54 -0700

    test

--- test.php
+++ test.php
@@ -50,7 +50,7 @@
 	textarea
 	{ width: 700px !important; }
 	input
-	{ margin-left: 10px; width: 200px !important; }
+	{ margin-left: 10px; width: 300px !important; }
 	button
 	{ float: right; width: 100px; }
 	#resultTable
