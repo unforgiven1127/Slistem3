d8ed9d9684fb1cdc2945a460eb1a9e167f8d1aea
unforgiven1127 <munir_anameric@hotmail.com>
2017-08-04 13:29:57 -0700

    test

--- mail_check.php
+++ mail_check.php
@@ -2,7 +2,7 @@
 
 require_once './common/lib/unirest/src/Unirest.php';
 
-$response = Unirest\Request::get("https://privatix-temp-mail-v1.p.mashape.com/request/mail/id/{md5}/",
+$response = Unirest\Request::get("https://privatix-temp-mail-v1.p.mashape.com/request/mail/id/514ccb46774bdbf6f07607a14d784c4c/",
   array(
     "X-Mashape-Key" => "OhHgw1CQBUmshJSITKjAoL1vY7qDp1rnYJWjsnF2rMg3A90AE8",
     "Accept" => "application/json"
