debe4c3f0488286ec5bfc69db1619f6989902dc8
unforgiven1127 <munir_anameric@hotmail.com>
2017-08-04 12:56:44 -0700

    test

--- mail_check.php
+++ mail_check.php
@@ -12,50 +12,12 @@
 
     $matches = array();
     $pattern = '/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i';
+
     preg_match_all($pattern,$html,$matches);
-    print_r($matches);
+    if(isset($matches[1]))
+    {
+        $email = $matches[1];
+    }
+    echo "email: $email";
 
 ?>
-
-<head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script>
-
-    
-</script>
-<style>
-	table
-	{ margin-left: 50px; margin-top: 50px; }
-	td
-	{ padding-top: 10px; }
-	textarea
-	{ width: 700px !important; }
-	input
-	{ margin-left: 10px; width: 300px !important; }
-	button
-	{ float: right; width: 100px; margin-left: 10px; }
-	#resultTable
-	{ display:none }
-	h4
-	{ font-weight: bold; }
-</style>
-</head>
-
-<body>
-    <form>
-    	<table>
-            <tr>
-                <td>
-                    <div id='links'></div>
-                </td>
-            </tr>
-    		<tr>
-    			<td>
-    				<button id='check' type="button" class="btn btn-warning">Check</button>
-    			</td>
-    		</tr>
-    	</table>
-
-    </form>
-</body>
