4a80eb35a308914fea7f9f70254e6039006aff3b
unforgiven1127 <munir_anameric@hotmail.com>
2017-08-04 19:47:43 -0700

    test

--- test.php
+++ test.php
@@ -6,7 +6,7 @@ require_once './common/lib/ChromePhp.php';
 		public $randomStringArray = array();
 
 		private function generateRandomString($length = 1024)
-		{
+		{//generates random string
 		    $chars = 'abcdefghijklmnopqrstuvwxyz';
 		    $charsLength = strlen($chars);
 
@@ -19,7 +19,7 @@ require_once './common/lib/ChromePhp.php';
 		}
 
 		static function returnStringArray($string = '')
-		{
+		{//gets a string and returns char array of this string
 			$returnArray = array();
 			$stringLen = strlen($string);
 			for($i = 0; $i < $stringLen; $i++)
@@ -30,7 +30,7 @@ require_once './common/lib/ChromePhp.php';
 		}
 
 		public function __construct()
-		{
+		{//constructor
 			$this->generateRandomString();
 		}
 	}
@@ -50,7 +50,7 @@ require_once './common/lib/ChromePhp.php';
 		public $errorFlag = false;
 
 		public function match($check1 = '', $check2 = '')
-		{
+		{// our primary function which compares both texts with eachother and with the random
 			$newRandom = new RandomString();
 			if($check1 == '' || $check2 == '')
 			{// to check 3 text with random generated text
@@ -65,7 +65,7 @@ require_once './common/lib/ChromePhp.php';
 				$check1Array = RandomString::returnStringArray($check1);
 				$check2Array = RandomString::returnStringArray($check2);
 				if(strlen($check1) == strlen($check2))
-				{
+				{//if texts have the same length
 					if($check1 == $check2)
 					{
 						$this->errorFlag = true;
@@ -74,28 +74,28 @@ require_once './common/lib/ChromePhp.php';
 					$texts = array('text1'=>$check2);
 				}
 				else if(strlen($check1) > strlen($check2))
-				{
+				{// search short one in the long one
 					$rsa = $check1Array;
 					$texts = array('text1'=>$check2);
 				}
 				else
-				{
+				{// search short one in the long one
 					$rsa = $check2Array;
 					$texts = array('text1'=>$check1);
 				}
 			}
 
     		foreach ($rsa as $key1 => $value)
-    		{
+    		{//just one pass for random text
     			foreach ($texts as $key2 => $text)
-    			{
+    			{// 3 pass for each text
     				$textArray = RandomString::returnStringArray($text);
 
 	    			if($textArray[0] == $value)
-	    			{
+	    			{// if we catch the first match
     					$controlString = $textArray[0];
     					for($i = 1; $i < strlen($text); $i++)
-    					{
+    					{//check the others
     						$newKey = $key1 + $i;
     						if(isset($rsa[$newKey]))
     						{
@@ -103,13 +103,13 @@ require_once './common/lib/ChromePhp.php';
     						}
     					}
     					if($controlString == $text)
-    					{
+    					{//we find the match...
     						if($this->internalCheckFlag)
-    						{
+    						{//3 text internal search
     							$this->errorFlag = true;
     						}
     						else
-    						{
+    						{//3 text vs random text search
     							if($key2 == 'text1'){$this->match1++;}
 	    						if($key2 == 'text2'){$this->match2++;}
 	    						if($key2 == 'text3'){$this->match3++;}
@@ -159,7 +159,6 @@ require_once './common/lib/ChromePhp.php';
 		    	$data['matches'] .= " <b>$text2</b> appears <b>".$newMacth->match2."</b> times,";
 		    	$data['matches'] .= " <b>$text3</b> appears <b>".$newMacth->match3."</b> times";
 	    	}
-
     	}
 		else
 	    {
@@ -171,8 +170,10 @@ require_once './common/lib/ChromePhp.php';
 ?>
 
 <head>
+<!-- I used bootstrap and JQuery -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!-- AJAX to pass data -->
 <script>
     $(function() {
         $('form').submit(function(e) {
@@ -188,6 +189,10 @@ require_once './common/lib/ChromePhp.php';
                 $('#result').html(data.random);
                 $('#resultTable').show();
                 $('#matches').html(data.matches);
+                if(data.error.length > 0)
+                {
+                	alert(data.error);
+                }
             })
         });
 
@@ -221,15 +226,15 @@ require_once './common/lib/ChromePhp.php';
     	<table>
     		<tr>
     			<td>Text 1: </td>
-    			<td><input type="text" class="form-control" name='text1' id="text1" maxlength="7" pattern="[a-z]{1,7}" title="The string should be less than 8 characters and lowercase"></td>
+    			<td><input type="text" class="form-control" name='text1' id="text1" maxlength="7" pattern="[a-z]{1,7}" title="The string should be less than 8 characters and lowercase"></td><!-- the lowercase and less than 8(max 7) letter restriction -->
     		</tr>
     		<tr>
     			<td>Text 2: </td>
-    			<td><input type="text" class="form-control" name='text2' id="text2" maxlength="7" pattern="[a-z]{1,7}" title="The string should be less than 8 characters and lowercase"></td>
+    			<td><input type="text" class="form-control" name='text2' id="text2" maxlength="7" pattern="[a-z]{1,7}" title="The string should be less than 8 characters and lowercase"></td><!-- the lowercase and less than 8(max 7) letter restriction -->
     		</tr>
     		<tr>
     			<td>Text 3: </td>
-    			<td><input type="text" class="form-control" name='text3' id="text3" maxlength="7" pattern="[a-z]{1,7}" title="The string should be less than 8 characters and lowercase"></td>
+    			<td><input type="text" class="form-control" name='text3' id="text3" maxlength="7" pattern="[a-z]{1,7}" title="The string should be less than 8 characters and lowercase"></td><!-- the lowercase and less than 8(max 7) letter restriction -->
     		</tr>
     		<tr>
     			<td colspan="2">
