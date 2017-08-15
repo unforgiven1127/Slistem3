9f4270e2c4f0dd24e943486f2da6239c137174cf
unforgiven1127 <munir_anameric@hotmail.com>
2017-08-14 18:38:37 -0700

    test

--- controls.php
+++ controls.php
@@ -154,13 +154,13 @@ function get_new_kpi_set()
 echo '<br><br>';
 //echo phpinfo();
 
-mysql_connect( DB_SERVER_SLISTEM, DB_USER_SLISTEM, DB_PASSWORD_SLISTEM) or die(mysql_error());
+$connection = mysql_connect( DB_SERVER_SLISTEM, DB_USER_SLISTEM, DB_PASSWORD_SLISTEM) or die(mysql_error());
 mysql_select_db(DB_NAME_SLISTEM) or die(mysql_error());
 
 
 $slistemQuery = "SELECT slc.* FROM sl_contact slc WHERE slc.sl_contactpk = (SELECT MAX(slc2.sl_contactpk) FROM sl_contact slc2 WHERE slc2.itemfk = slc.itemfk AND slc2.type = 2) and slc.sl_contactpk > 709572";//belirli sayida aliyor dikkat
 
-$slistemQuery = mysqli_query($slistemQuery);
+$slistemQuery = mysqli_query($connection,$slistemQuery);
 
 $count = 0;
 
