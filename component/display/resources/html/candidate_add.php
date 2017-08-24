afa3862f96ecb1868ad0648aeceab31494556874
unforgiven1127 <munir_anameric@hotmail.com>
2017-08-24 15:19:35 -0700

    list

--- component/display/resources/html/candidate_add.php
+++ component/display/resources/html/candidate_add.php
@@ -84,7 +84,7 @@
 						<?php echo $nationality; ?>
 						</select>
 					</div>
-					<div class="general_form_label">Location</div>
+					<div class="general_form_label add_margin_left_30">Location</div>
 					<div class="general_form_column" style="width: 183px;">
 					<?php echo $occupation_tree; ?>
 					</div>
