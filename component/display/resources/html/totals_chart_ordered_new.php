86833f383d880b9f0dd8db9e432b6c30bf8960b4
unforgiven1127 <munir_anameric@hotmail.com>
2017-09-04 21:44:40 -0700

    KPI export

--- component/display/resources/html/totals_chart_ordered_new.php
+++ component/display/resources/html/totals_chart_ordered_new.php
@@ -52,7 +52,14 @@
 		}, 30000);
 	}
 
+	$(function () {
+		$("#exportToExcel").click(function(){
+		  alert('test');
+		});
+	});
 </script>
+
+
 
 <style>
 .table-striped > tbody > tr:nth-child(2n+1) > td, .table-striped > tbody > tr:nth-child(2n+1) > th {
