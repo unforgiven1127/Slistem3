f553fabd15d89b4ee1ca1fec90f90da7f1fe2cea
unforgiven1127 <munir_anameric@hotmail.com>
2017-12-07 15:40:33 -0800

    top menu

--- checkMeetings.php
+++ checkMeetings.php
@@ -36,9 +36,11 @@
         $new_month = date("m",strtotime($effectiveDate));
         $control_date = $year.'-'.$new_month.'-'.'06 00:00:00';
 
+        $final = date("Y-m-d", strtotime("+1 month", $create_date));
+
         $today = date("Y-m-d H:i:s");
             echo $create_date.' ---- ';
-            echo date("Y-m-t", strtotime($control_date));
+            echo date("Y-m-t", strtotime($final));
             echo '<br>';
         if($meeting['meeting_done'] == 0 && strtotime($today) >= strtotime($control_date ) )
         {
