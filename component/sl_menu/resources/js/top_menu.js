bfef90bfc5452f5804d0b5c54621fe0406ec3380
unforgiven1127 <munir_anameric@hotmail.com>
2017-12-16 15:50:35 -0800

    beta

--- component/sl_menu/resources/js/top_menu.js
+++ component/sl_menu/resources/js/top_menu.js
@@ -1,6 +1,7 @@
 var element = $(window).width();
 var half = (element/3)*2;
 var count = 0;
+var change_flag = 0;
 //ert($element);
 function checkForChanges()
 {
@@ -15,27 +16,25 @@ function checkForChanges()
 		var reports = document.getElementById('reports').innerHTML;
 		var contact_sheet = document.getElementById('contact_sheet').innerHTML;
 		var tools = document.getElementById('tools').innerHTML;
-alert(messaging);
 		count = 1;
 	}
 
     if (lastHeight < half)
     {
-    	//alert('here');
-    	//alert(document.getElementById('scheduler').innerHTML.replace(" Scheduler", "S"));
+    	change_flag = 1;
     	var scheduler1 = document.getElementById('scheduler').innerHTML.replace(" Scheduler", "|");
     	document.getElementById('scheduler').innerHTML = scheduler1;
     }
-    else
+    else if(count == 1 && change_flag == 1)
     {
     	//alert('here1');
-    	//document.getElementById('messaging').innerHTML = messaging;
-    	//document.getElementById('scheduler').innerHTML = scheduler;
-    	//document.getElementById('add').innerHTML = add;
-    	//document.getElementById('view').innerHTML = view;
-    	//document.getElementById('reports').innerHTML = reports;
-    	//document.getElementById('contact_sheet').innerHTML = contact_sheet;
-    	//document.getElementById('tools').innerHTML = tools;
+    	document.getElementById('messaging').innerHTML = messaging;
+    	document.getElementById('scheduler').innerHTML = scheduler;
+    	document.getElementById('add').innerHTML = add;
+    	document.getElementById('view').innerHTML = view;
+    	document.getElementById('reports').innerHTML = reports;
+    	document.getElementById('contact_sheet').innerHTML = contact_sheet;
+    	document.getElementById('tools').innerHTML = tools;
     }
 
     setTimeout(checkForChanges, 500);
