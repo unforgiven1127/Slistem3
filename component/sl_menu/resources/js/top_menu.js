959b3adb9a951c745ec79d545cf2488160d50931
unforgiven1127 <munir_anameric@hotmail.com>
2017-12-16 15:38:09 -0800

    beta

--- component/sl_menu/resources/js/top_menu.js
+++ component/sl_menu/resources/js/top_menu.js
@@ -1,34 +1,38 @@
 var element = $(window).width();
 var half = (element/3)*2;
+var count = 0;
 //ert($element);
 function checkForChanges()
 {
-	var messaging = 'Messaging';
-	var scheduler = 'Scheduler';
-	var add = 'Add';
-	var view = 'View';
-	var reports = 'Reports';
-	var contact_sheet ='Contact sheet';
-	var tools = 'Tools';
-
-	var menu_items = $('.mainMenuItems');
-
 	var lastHeight = $(window).width();
+	if(count == 0 && lastHeight > half)
+	{
+		var messaging = document.getElementById('messaging').innerHTML;
+		var scheduler = document.getElementById('scheduler').innerHTML;
+		var add = document.getElementById('add').innerHTML;
+		var view = document.getElementById('view').innerHTML;
+		var reports = document.getElementById('reports').innerHTML;
+		var contact_sheet = document.getElementById('contact_sheet').innerHTML;
+		var tools = document.getElementById('tools').innerHTML;
+
+		count = 1;
+	}
+
     if (lastHeight < half)
     {
     	alert(document.getElementById('scheduler').innerHTML.replace(" Scheduler", "S"));
     	var scheduler1 = document.getElementById('scheduler').innerHTML.replace(" Scheduler", "|");
     	document.getElementById('scheduler').innerHTML = scheduler1;
     }
-    else if($('#messaging').text() == '')
+    else
     {
-    	$('#messaging').text(messaging);
-    	$('#scheduler').text(scheduler);
-    	$('#add').text(add);
-    	$('#view').text(view);
-    	$('#reports').text(reports);
-    	$('#contact_sheet').text(contact_sheet);
-    	$('#tools').text(tools);
+    	document.getElementById('messaging').innerHTML = messaging;
+    	document.getElementById('scheduler').innerHTML = scheduler;
+    	document.getElementById('add').innerHTML = add;
+    	document.getElementById('view').innerHTML = view;
+    	document.getElementById('reports').innerHTML = reports;
+    	document.getElementById('contact_sheet').innerHTML = contact_sheet;
+    	document.getElementById('tools').innerHTML = tools;
     }
 
     setTimeout(checkForChanges, 500);
