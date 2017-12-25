dc2b6da2904542fbf142447342dcb39110111f2c
unforgiven1127 <munir_anameric@hotmail.com>
2017-12-25 11:54:47 -0800

    beta

--- component/display/resources/html/complex_search.php
+++ component/display/resources/html/complex_search.php
@@ -1,14 +1,12 @@
-<table style='padding-top: 10px !important; padding-right: 10px !important;'>
+<table>
 	<tr>
 		<td>
-
-			<div class="searchActionBar">
+			<div  style='padding-top: 10px !important; padding-right: 10px !important;' class="searchActionBar">
 					<div class="formMenuItem savedSearch" onclick="AjaxRequest('https://beta2.slate.co.jp/index.php5?uid=898-775&amp;ppa=ppasea&amp;ppt=&amp;ppk=0&amp;formType=advanced&amp;CpUid=555-001&amp;CpType=candi&amp;pg=ajx&amp;formType=complex&amp;formType=global&amp;formType=complex&amp;formType=advanced&amp;formType=history', '', '', $(this).closest('.ui-dialog-content').attr('id'));">&nbsp;</div>
 					<div class="formMenuItem candidate" onclick="AjaxRequest('https://beta2.slate.co.jp/index.php5?uid=898-775&amp;ppa=ppasea&amp;ppt=&amp;ppk=0&amp;formType=advanced&amp;CpUid=555-001&amp;CpType=candi&amp;pg=ajx&amp;formType=complex&amp;formType=global&amp;formType=complex&amp;formType=advanced&amp;CpType=candi&amp;formType=advanced', '', '', $(this).closest('.ui-dialog-content').attr('id'));">&nbsp;</div>
 					<div class="formMenuItem company" onclick="AjaxRequest('https://beta2.slate.co.jp/index.php5?uid=898-775&amp;ppa=ppasea&amp;ppt=&amp;ppk=0&amp;formType=advanced&amp;CpUid=555-001&amp;CpType=candi&amp;pg=ajx&amp;formType=complex&amp;formType=global&amp;formType=complex&amp;formType=advanced&amp;CpType=comp&amp;formType=advanced', '', '', $(this).closest('.ui-dialog-content').attr('id'));">&nbsp;</div>
 					<div class="searchFormSwitchMode global" onclick="AjaxRequest('https://beta2.slate.co.jp/index.php5?uid=898-775&amp;ppa=ppasea&amp;ppt=&amp;ppk=0&amp;formType=advanced&amp;CpUid=555-001&amp;CpType=candi&amp;pg=ajx&amp;formType=complex&amp;formType=global&amp;formType=complex&amp;formType=advanced&amp;formType=global', '', '', $(this).closest('.ui-dialog-content').attr('id'));">&nbsp;</div>
 					<div class="searchFormSwitchMode advanced" onclick="AjaxRequest('https://beta2.slate.co.jp/index.php5?uid=898-775&amp;ppa=ppasea&amp;ppt=&amp;ppk=0&amp;formType=advanced&amp;CpUid=555-001&amp;CpType=candi&amp;pg=ajx&amp;formType=complex&amp;formType=advanced', '', '', $(this).closest('.ui-dialog-content').attr('id'));">&nbsp;</div>
-
 		</td>
 	</tr>
 </table>
