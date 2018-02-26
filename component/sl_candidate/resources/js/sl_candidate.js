18fe8b44c6d8766959d91a2a9ac7378f1cd94042
unforgiven1127 <munir_anameric@hotmail.com>
2018-02-25 19:59:21 -0800

    beta

--- component/sl_candidate/resources/js/sl_candidate.js
+++ component/sl_candidate/resources/js/sl_candidate.js
@@ -2,7 +2,7 @@
 function toggleCandiTab(poElement, psIdToShow, psContainer, candidate_id,is_client = false)
 {
   //psIdToShow acilacak olan tab
-
+Console.log(psIdToShow);
   if(psIdToShow == 'candiTab2' &&  is_client != false &&  is_client != 'false')
   {
     if(confirm("This candidate works at a Slate client, if you access his contact information, a notification will be sent to the client\'s owner."))
@@ -45,8 +45,7 @@ function toggleCandiTab(poElement, psIdToShow, psContainer, candidate_id,is_clie
           psContainer = '';
 
         var oElement = $(psContainer+' .aTabContent:visible', oContainer);
-Console.log(psIdToShow);
-Console.log(oContainer);
+
         $(oElement).fadeOut(0, function()
         {
           $('#'+psIdToShow, oContainer).fadeIn(325, function()
